eb740842e1f4da4b84881912135925d4863399fc
Cykyrios <cykyrios@gmail.com>
2020-05-29 19:37:41 +0200

    Move demo scene code out of Camera360.gd

--- Camera360/Camera360.gd
+++ Camera360/Camera360.gd
@@ -5,7 +5,6 @@ class_name Camera360
 export (float, 10, 360) var fovx = 150 setget set_fovx
 export (int, "Rectilinear", "Panini", "Fisheye", "Stereographic", "Cylindrical",
 		"Equirectangular", "Mercator") var lens = 0 setget set_lens
-export (bool) var show_grid = false setget set_show_grid
 export (int, 1, 16384) var camera_resolution = 1080
 export (float, 0.001, 10) var clip_near = 0.1
 export (float, 0.01, 10000) var clip_far = 1000
@@ -17,8 +16,6 @@ var cameras = []
 
 var render_quad: MeshInstance = null
 var mat = load("res://Camera360/Camera360.tres")
-
-var grid = load("res://Camera360/Grid.png")
 
 
 func _ready():
@@ -34,8 +31,6 @@ func _ready():
 	mat.set_shader_param("fovx", fovx)
 	mat.set_shader_param("projection", lens)
 	mat.set_shader_param("resolution", get_viewport().size)
-	mat.set_shader_param("Grid", grid)
-	mat.set_shader_param("show_grid", show_grid)
 	
 	for i in range(num_cameras):
 		var viewport = Viewport.new()
@@ -57,8 +52,6 @@ func _ready():
 	if num_cameras < 6:
 		for i in range(num_cameras + 1, 6):
 			mat.set_shader_param("Texture%d" % [i], Texture.new())
-	
-	update_projection_label()
 
 
 func _process(delta):
@@ -75,18 +68,6 @@ func _process(delta):
 		cameras[5].rotate_object_local(Vector3.UP, PI)
 
 
-func _input(event):
-	if event is InputEventKey:
-		if event.scancode == KEY_G and event.pressed:
-			self.show_grid = !show_grid
-		elif event.scancode == KEY_P and event.pressed:
-			self.lens = lens + 1
-		elif event.scancode == KEY_KP_ADD and event.pressed:
-			self.fovx = fovx + 5
-		elif event.scancode == KEY_KP_SUBTRACT and event.pressed:
-			self.fovx = fovx - 5
-
-
 func set_fovx(x: float):
 	fovx = x
 	if fovx < 10:
@@ -94,37 +75,10 @@ func set_fovx(x: float):
 	if fovx > 360:
 		fovx = 360
 	mat.set_shader_param("fovx", fovx)
-	call_deferred("update_projection_label")
 
 
-func set_show_grid(show: bool):
-	show_grid = show
-	mat.set_shader_param("show_grid", show_grid)
-
-
-func set_lens(proj: int):
-	lens = proj
+func set_lens(l: int):
+	lens = l
 	if lens > 6:
 		lens = 0
 	mat.set_shader_param("projection", lens)
-	call_deferred("update_projection_label")
-
-
-func update_projection_label():
-	var proj: String
-	match lens:
-		0:
-			proj = "Rectilinear"
-		1:
-			proj = "Panini"
-		2:
-			proj = "Fisheye"
-		3:
-			proj = "Stereographic"
-		4:
-			proj = "Cylindrical"
-		5:
-			proj = "Equirectangular"
-		6:
-			proj = "Mercator"
-	$ProjectionLabel.text = "Projection: %s\nFoV: %d°" % [proj, fovx]
--- DemoScene.gd
+++ DemoScene.gd
@@ -5,9 +5,32 @@ onready var camera = $Camera360
 
 var speed = 0.0
 
+export (bool) var show_grid = false setget set_show_grid
+var grid = load("res://Camera360/Grid.png")
+
+
+func _ready():
+	update_projection_label()
+	
+	var mat = camera.render_quad.mesh.surface_get_material(0)
+	mat.set_shader_param("Grid", grid)
+	mat.set_shader_param("show_grid", show_grid)
+
 
 func _input(event):
-	if event is InputEventMouseButton:
+	var is_projection_label_outdated := false
+	if event is InputEventKey:
+		if event.scancode == KEY_G and event.pressed:
+			self.show_grid = !show_grid
+		elif event.scancode == KEY_P and event.pressed:
+			camera.lens += 1
+		elif event.scancode == KEY_KP_ADD and event.pressed:
+			camera.fovx += 5
+		elif event.scancode == KEY_KP_SUBTRACT and event.pressed:
+			camera.fovx -= 5
+		is_projection_label_outdated = true
+	
+	elif event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			speed += 0.25
 		elif event.button_index == BUTTON_RIGHT and event.pressed:
@@ -18,7 +41,36 @@ func _input(event):
 			camera.fovx -= 5
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			camera.fovx += 5
+		is_projection_label_outdated = true
+	
+	if is_projection_label_outdated:
+		call_deferred("update_projection_label")
 
 
 func _process(delta):
 	camera.rotate(Vector3.UP, speed * delta)
+
+
+func set_show_grid(show: bool):
+	show_grid = show
+	camera.render_quad.mesh.surface_get_material(0).set_shader_param("show_grid", show_grid)
+
+
+func update_projection_label():
+	var proj: String
+	match camera.lens:
+		0:
+			proj = "Rectilinear"
+		1:
+			proj = "Panini"
+		2:
+			proj = "Fisheye"
+		3:
+			proj = "Stereographic"
+		4:
+			proj = "Cylindrical"
+		5:
+			proj = "Equirectangular"
+		6:
+			proj = "Mercator"
+	$ProjectionLabel.text = "Projection: %s\nFoV: %d°" % [proj, camera.fovx]
