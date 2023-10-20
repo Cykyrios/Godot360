208d2508a88d99dc6bfff0ea2b3ebd2b981a120e
Cykyrios <cykyrios@gmail.com>
2023-10-20 23:00:19 +0200

    Port project to Godot 4

---
+++ src/camera360/camera360.gd
@@ -0,0 +1,98 @@
+class_name Camera360
+extends Camera3D
+
+
+enum Lens {RECTILINEAR, PANINI, FISHEYE, STEREOGRAPHIC, CYLINDRICAL, EQUIRECTANGULAR, MERCATOR}
+
+@export_range(10, 360) var fovx := 150.0: set = set_fovx
+@export var lens := Lens.RECTILINEAR: set = set_lens
+@export_range(1, 16384) var camera_resolution := 1080
+@export_range(0.001, 10) var clip_near := 0.1
+@export_range(0.01, 10000) var clip_far := 1000.0
+@export_range(1, 6) var num_cameras := 6
+@export_range(1, 20) var render_layer := 11
+@export var camera_environment: Environment = null
+
+var viewports : Array[SubViewport] = []
+var cameras : Array[Camera3D] = []
+
+var render_quad: MeshInstance3D = null
+var mat := ShaderMaterial.new()
+
+
+func _ready() -> void:
+	render_layer = int(pow(2, render_layer - 1))
+	cull_mask = render_layer
+
+	render_quad = MeshInstance3D.new()
+	add_child(render_quad)
+	render_quad.translate_object_local(Vector3.FORWARD * (near + 0.1 * (far - near)))
+	var quad_mesh := QuadMesh.new()
+	quad_mesh.size = Vector2(2, 2)
+	render_quad.mesh = quad_mesh
+	render_quad.layers = render_layer
+	render_quad.mesh.surface_set_material(0, mat)
+
+	mat.shader = preload("res://src/camera360/camera360.gdshader")
+	mat.set_shader_parameter("fovx", fovx)
+	mat.set_shader_parameter("lens", lens)
+	mat.set_shader_parameter("resolution", get_viewport().size)
+
+	for i in num_cameras:
+		var viewport := SubViewport.new()
+		add_child(viewport)
+		viewport.size = camera_resolution * Vector2.ONE
+		viewport.positional_shadow_atlas_size = 4096
+		viewport.msaa_3d = SubViewport.MSAA_4X
+		viewports.append(viewport)
+		mat.set_shader_parameter("Texture%d" % [i], viewport.get_texture())
+
+		var camera := Camera3D.new()
+		viewport.add_child(camera)
+		camera.fov = 90
+		camera.near = clip_near
+		camera.far = clip_far
+		camera.cull_mask -= render_layer
+		cameras.append(camera)
+
+	if num_cameras < 6:
+		for i in range(num_cameras + 1, 6):
+			mat.set_shader_parameter("Texture%d" % [i], Texture2D.new())
+
+
+func _process(_delta: float) -> void:
+	for camera in cameras:
+		camera.global_transform = global_transform
+	if num_cameras >= 2:
+		cameras[1].rotate_object_local(Vector3.UP, PI/2)
+	if num_cameras >= 3:
+		cameras[2].rotate_object_local(Vector3.UP, -PI/2)
+	if num_cameras >= 4:
+		cameras[3].rotate_object_local(Vector3.RIGHT, -PI/2)
+	if num_cameras >= 5:
+		cameras[4].rotate_object_local(Vector3.RIGHT, PI/2)
+	if num_cameras >= 6:
+		cameras[5].rotate_object_local(Vector3.UP, PI)
+
+
+func set_fovx(x: float) -> void:
+	fovx = x
+	if fovx < 10:
+		fovx = 10
+	if fovx > 360:
+		fovx = 360
+	mat.set_shader_parameter("fovx", fovx)
+
+
+func set_lens(l: Lens) -> void:
+	lens = l
+	if lens > Lens.size() - 1:
+		lens = Lens.RECTILINEAR
+	mat.set_shader_parameter("lens", lens)
+
+
+func activate_next_lens() -> void:
+	var next_lens := Lens.values().find(lens) + 1
+	if next_lens >= Lens.values().size():
+		next_lens = 0
+	set_lens(Lens.values()[next_lens])
---
+++ src/demo_scene.gd
@@ -0,0 +1,77 @@
+extends Node3D
+
+
+@onready var camera := %Camera360 as Camera360
+@onready var projection_label := %ProjectionLabel as Label
+
+var speed := 0.0
+
+@export var show_grid := false:
+	set(value):
+		show_grid = value
+		if not is_inside_tree():
+			await ready
+		camera.render_quad.mesh.surface_get_material(0).set_shader_parameter("show_grid", show_grid)
+var grid := preload("res://assets/grid.png")
+
+
+func _ready() -> void:
+	update_projection_label()
+
+	var mat := camera.render_quad.mesh.surface_get_material(0) as ShaderMaterial
+	mat.set_shader_parameter("Grid", grid)
+	mat.set_shader_parameter("show_grid", show_grid)
+
+
+func _input(event: InputEvent) -> void:
+	var is_projection_label_outdated := false
+	if event is InputEventKey:
+		if event.keycode == KEY_G and event.pressed:
+			self.show_grid = not show_grid
+		elif event.keycode == KEY_L and event.pressed:
+			camera.activate_next_lens()
+		elif event.keycode == KEY_KP_ADD and event.pressed:
+			camera.fovx += 5
+		elif event.keycode == KEY_KP_SUBTRACT and event.pressed:
+			camera.fovx -= 5
+		is_projection_label_outdated = true
+
+	elif event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			speed += 0.25
+		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			speed -= 0.25
+		speed = clampf(speed, -1.0, 1.0)
+
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			camera.fovx -= 5
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			camera.fovx += 5
+		is_projection_label_outdated = true
+
+	if is_projection_label_outdated:
+		call_deferred("update_projection_label")
+
+
+func _process(delta: float) -> void:
+	camera.rotate(Vector3.UP, speed * delta)
+
+
+func update_projection_label() -> void:
+	var proj := ""
+	match camera.lens:
+		Camera360.Lens.RECTILINEAR:
+			proj = "Rectilinear"
+		Camera360.Lens.PANINI:
+			proj = "Panini"
+		Camera360.Lens.FISHEYE:
+			proj = "Fisheye"
+		Camera360.Lens.STEREOGRAPHIC:
+			proj = "Stereographic"
+		Camera360.Lens.CYLINDRICAL:
+			proj = "Cylindrical"
+		Camera360.Lens.EQUIRECTANGULAR:
+			proj = "Equirectangular"
+		Camera360.Lens.MERCATOR:
+			proj = "Mercator"
+	projection_label.text = "Lens: %s\nFoV: %d°" % [proj, camera.fovx]
